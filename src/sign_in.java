80d69520e2f581a303241183162df2998ff41680
vignesh5373@gmail.com <vignesh.sivamurthi@tesco.com>
2020-10-10 16:48:35 +0530

    Bank registration,sign in

--- src/sign_in.java
+++ src/sign_in.java
@@ -3,21 +3,90 @@
  */
 import java.util.Scanner;
 public class sign_in {
+    static Scanner object = new Scanner(System.in);
 
-    public static void signin() {
-        Scanner object = new Scanner(System.in);
-        System.out.println("Enter your user name:");
+    public static void registartion() {
+
+        //input fields from registration page
+
+        //Email address field
+
+        System.out.println("\nEnter your registered email address:");
+        String Email = object.nextLine();
+
+
+//user name field
+
+        System.out.println("\n Enter your user name:");
         String user_name = object.nextLine();
-        System.out.println("Enter your password:");
+
+
+        //Password field
+
+        System.out.println("\n Create your password:");
         String password = object.nextLine();
-        System.out.println("\n User name:" + user_name);
-        System.out.println("\n Password:" + password);
+        System.out.println("\n Confirm your password:");
+        String Confirm_password = object.nextLine();
+
+        //Mobile number field
+
+        System.out.print("\n Enter your registered phone number:");
+        Long phone = object.nextLong();
+
+        //Submitting all details
+
+        if(Email.equals("vignesh@gmail.com")&& user_name.equals("vicky")&& phone.equals(978948)) {
+            System.out.println("Registartion is successful");
+        }else
+        {
+            System.out.println("Registartion is unsuccessful");
+
+        }
+
     }
+    //Sign in flow
+
+    public static void signin()
+    {
+        System.out.println("\nEnter your registered email address:");
+        String Email = object.nextLine();
+        System.out.println("\nEnter your password:");
+        String password = object.nextLine();
+
+        //Two factor authentication
+        //Checking user name and password
+
+        if(Email.equals("vignesh@gmail.com")&& password.equals("vicky@15")) {
+            System.out.println(" Login successful \n Enter your OTP number to sent your mobile number: ");
+        }
+        else{
+            System.out.println("Incoreect user name or password");
+        }
+
+        //Checking OTP
+
+        int OTP = object.nextInt();
+
+        if(OTP==1234)
+        {
+            System.out.println("Home page");
+        }
+        else{
+            System.out.println("Incorrect OTP");
+        }
+
+
+    }
+
+    public static void acc_details(String user_name,String Email,long phone,int acc_num,double balance,String acc_type){
+
+        System.out.println("\nuser name:"+user_name+"\nEmail address:"+Email+"\nPhone number:"+phone+"\nAccount number:"+acc_num+"\nAmount balance:"+balance+"Account type:"+acc_type);
+    }
+
     public static void main(String[] args) {
 
         sign_in.signin();
-        System.out.println("\n login is successful");
+
+
     }
-
 }
-
--- src/switch_caluclator.java
+++ src/switch_caluclator.java
@@ -3,17 +3,17 @@
  */
 public class switch_caluclator {
     public static void calculator(double a, double b) {
-        String operation="division";
+        String operation="";
         switch (operation) {
             case "Addition":
                 System.out.println("Addition of two number is:" + (a + b));
-                break;
+
             case "Subtration":
                 System.out.println("substraction of two number is:" + (a - b));
-                break;
+
             case "division":
                 System.out.println("division of two number is:" + (a / b));
-                break;
+
             default:
                 System.out.println("Not performed any operation");
                 break;
